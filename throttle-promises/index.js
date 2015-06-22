f31d5fbdcbfbb882559d42b4df7ab28a44dcae1c
Jeffrey Barrus <jeffrey.barrus@fmr.com>
2015-06-22 14:30:26 -0600

    started throttle-promises

--- throttle-promises/index.js
+++ throttle-promises/index.js
@@ -1,32 +1,32 @@
-var Promise = Promise | require('../node_modules/promise-polyfill/Promise');
+var Promise = Promise || require('../node_modules/promise-polyfill/Promise');
 
 module.exports = function(max, functions) {
-  console.log('max', max);
+  var promise = new Promise(function(resolve) {
+    var results = [];
+    var running = 0;
 
-  return new Promise(function(resolve) {
-    //var results = [];
-    //var running = 0;
-    //
-    //queue();
-    //queue();
-    //queue();
-    //queue();
-    //queue();
-    //queue();
-    //queue();
-    //
-    //function queue() {
-    //  if (running < max) {
-    //    running++;
-    //    functions.shift()().then(function(result) {
-    //      running--;
-    //      results.push(result);
-    //
-    //      if (running === 0) {
-    //        resolve(results);
-    //      }
-    //    });
-    //  }
-    //}
+    for (var i=0; i<max; i++) {
+      queue();
+    }
+
+    function queue() {
+      if (running < max && functions.length) {
+        running++;
+        //console.log('queue, running=', running);
+        functions.shift()().then(function(result) {
+          running--;
+          //console.log('took one, left=', functions.length, 'running=', running);
+          results.push(result);
+
+          if (running === 0) {
+            resolve(results);
+          } else {
+            queue();
+          }
+        });
+      }
+    }
   });
+
+  return promise;
 };
